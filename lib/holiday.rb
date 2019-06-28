a161ad6605bf58ee5c60d596989183832f246bee
Motoaki Sumi <msumi@mba2019.hbs.edu>
2019-06-28 10:19:11 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -85,7 +85,7 @@ def all_holidays_with_bbq(holiday_hash)
   
   holiday_hash.each do |season, holidays|
     holidays.each do |holiday, supplies|
-      if supplies.includ?("BBQ")
+      if supplies.include?("BBQ")
         holiday_array << holiday
       end
     end
